f1cac18f5a39d045ee18caa966d0df794036cab8
Aivar Annamaa <aivar.annamaa@gmail.com>
2022-08-14 19:48:28 +0300

    Version 1.0b4

---
+++ create_dist.bat
@@ -0,0 +1,12 @@
+set PATH=C:\Python310-64\Scripts;%PATH%
+rmdir build /s /q
+
+@echo ............... CREATING wheel ................................
+python setup.py bdist_wheel
+
+@echo ............... CREATING sdist ................................
+python setup.py sdist --formats=gztar
+
+cd packaging\setuptools
+
+rmdir pipkin.egg-info /s /q
